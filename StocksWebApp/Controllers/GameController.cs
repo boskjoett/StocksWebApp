af9dbae538e657760ed270ad927c385854d97b5c
Bo Christian Skjøtt <bo@skjoett.com>
2018-12-24 13:53:12 +0100

    First commit

---
+++ StocksWebApp/Controllers/GameController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StocksWebApp.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StartGame()
+        {
+            return View("Game");
+        }
+    }
+}
